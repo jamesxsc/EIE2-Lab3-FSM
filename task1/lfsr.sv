47d965171b918609451cc5c7aa9ba33378864ed4
eliott <eliott.chestnut@gmail.com>
2024-11-01 11:18:20 +0100

    hesomehowknowswindowsbetterthanme

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -1,8 +1,18 @@
-module lfsr(
-    input   logic       clk,
-    input   logic       rst,
+
+module lfsr4 (
+    input logic        clk,        // clock
+    input logic        rst,        // reset
     input   logic       en,
-    output  logic [3:0] data_out
+    output logic [4:1] data_out    // pseudo-random output
 );
 
+    always_ff @ (posedge clk, posedge rst)
+        if (rst)
+            sreg <= 4'b1;
+        else if (en)
+            sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
+        else
+            sreg<=sreg
+
+    assign data_out = sreg;
 endmodule
